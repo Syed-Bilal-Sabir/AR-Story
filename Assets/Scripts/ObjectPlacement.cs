a9339a1440391fd50f58d7cfe5a3381bb101abd0
Syed Bilal Sabir <bilalsabir709@gmail.com>
2022-12-13 16:26:39 +0000

    progressed

--- Assets/Scripts/ObjectPlacement.cs
+++ Assets/Scripts/ObjectPlacement.cs
@@ -4,17 +4,56 @@ using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 
+[RequireComponent(typeof(ARTrackedImageManager))]
 public class ObjectPlacement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private ARTrackedImageManager aRTrackedImageManager;
+    private readonly Dictionary<string,GameObject> InstantiatedArPrefab = new Dictionary<string, GameObject>();
+    public GameObject[] ARPrefabs;
+
+    private void Awake()
     {
+        aRTrackedImageManager = GetComponent<ARTrackedImageManager>();
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
+    {
+        aRTrackedImageManager.trackedImagesChanged += OnTrackedImageChanged;
+    }
+    private void OnDisable()
+    {
+        aRTrackedImageManager.trackedImagesChanged -= OnTrackedImageChanged;
+    }
+
+    private void OnTrackedImageChanged(ARTrackedImagesChangedEventArgs Args)
     {
         
+        foreach (var image in Args.added)
+        {
+            var ImgName = image.referenceImage.name;
+
+
+            foreach (var currentPrefab in ARPrefabs)
+            {
+                if (string.Compare(currentPrefab.name, ImgName,System.StringComparison.OrdinalIgnoreCase) == 0 && !InstantiatedArPrefab.ContainsKey(ImgName))
+                {
+                    var CreatePrefab = Instantiate(currentPrefab,image.transform);
+                    InstantiatedArPrefab[ImgName] = CreatePrefab;
+                }
+            }
+        }
+        //image  changed
+        foreach(var image in Args.updated)
+        {
+            InstantiatedArPrefab[image.referenceImage.name].SetActive(image.trackingState == TrackingState.Tracking);
+        }
+        //image reemoved from scene
+        foreach (var image in Args.removed)
+        {
+            Destroy(InstantiatedArPrefab[image.referenceImage.name]);
+            InstantiatedArPrefab.Remove(image.referenceImage.name);
+        }
     }
+    
 }
